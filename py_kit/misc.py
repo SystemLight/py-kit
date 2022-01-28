5a43bf3e195dfebd6670f0fbc9c9ec7680bb4c19
SystemLight <1466335092@qq.com>
2022-01-28 09:32:14 +0800

    feat: EventEmitter增加one方法

--- py_kit/misc.py
+++ py_kit/misc.py
@@ -299,8 +299,22 @@ class EventEmitter:
 
         return decorator
 
-    def add_event_listener(self, key: str, callback: Callable):
-        self._event_pool[key].append(callback)
+    def one(self, key: str):
+        def decorator(func: Callable):
+            self.add_event_listener(key, func, True)
+            return func
+
+        return decorator
+
+    def add_event_listener(self, key: str, callback: Callable, is_one=False):
+        if is_one:
+            def one_callback(*args):
+                callback(*args)
+                self._event_pool[key].remove(one_callback)
+
+            self._event_pool[key].append(one_callback)
+        else:
+            self._event_pool[key].append(callback)
 
     def remove_event_listener(self, key: str, callback: Callable):
         self._event_pool[key].remove(callback)
@@ -312,5 +326,3 @@ class EventEmitter:
             args = []
         for func in self._event_pool[key]:
             func(*args)
-
-
