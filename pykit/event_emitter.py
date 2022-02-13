f32f9497320e2aef956719032871e72ce2e0560d
SystemLight <1466335092@qq.com>
2022-02-13 23:38:46 +0800

    fix: 修复EventEmitter派发错误问题

--- pykit/event_emitter.py
+++ pykit/event_emitter.py
@@ -1,5 +1,7 @@
+import copy
+import time
 from collections import defaultdict
-from typing import Callable
+from typing import Callable, Optional
 
 
 class EventEmitter:
@@ -37,10 +39,60 @@ class EventEmitter:
         self._event_pool[key].remove(callback)
 
     def remove_key(self, key: str):
+        """
+
+        移除指定key值的所有监听器
+
+        :param key:
+        :return:
+
+        """
         self._event_pool.pop(key)
 
     def emit(self, key: str, args=None):
         if args is None:
             args = []
-        for func in self._event_pool[key]:
+        for func in copy.copy(self._event_pool[key]):  # 防止回调函数中含有删除或者添加方法导致错误
             func(*args)
+
+    def get(self, key: str, timeout=0, sleep=0.5) -> tuple:
+        """
+
+        提供一种同步阻塞的方式去监听key的返回值
+
+        :param key: 监听key值
+        :param timeout: 超时
+        :param sleep: 延迟捕获
+        :return:
+
+        """
+        result_args: Optional[tuple] = None
+        timeout_flag = False
+        start_time = time.time()
+
+        def _callback(*args):
+            """
+
+            其它线程调取
+
+            :param args:
+            :return:
+
+            """
+            nonlocal result_args
+            result_args = args
+
+        self.add_event_listener(key, _callback)
+
+        while not result_args:
+            if 0 < timeout < (time.time() - start_time):
+                timeout_flag = True
+                break
+            time.sleep(sleep)
+
+        self.remove_event_listener(key, _callback)
+
+        if timeout_flag:
+            raise TimeoutError
+
+        return result_args
